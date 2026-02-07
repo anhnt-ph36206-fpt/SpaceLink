86f28a37ccab7a73beba568536bcdc57d779a262
Anh <anhntph36206@fpt.edu.vn>
2026-02-07 12:14:19 +0700

    feat(catalog): Thêm vào mig + model + seeders 3 bảng Brands, Category, AttributeGroup, thiết lập mối  quan hệ Models 3 bảng

---
+++ backend/app/Models/Brand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'logo',
+        'description',
+        'is_active',
+        'display_order',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    /**
+     * Relationships
+     */
+
+    // Brand has many products
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
