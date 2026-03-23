098c401b1ec29dbae75d3816e8619891c5ed7a31
Vu Hoang Hiep <vhghip2905@gmail.com>
2026-03-23 19:05:21 +0700

    update banner hip

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,39 +1,29 @@
-import React from 'react';
-import HeroSlider from '../components/home/HeroSlider';
-import ServicesSection from '../components/home/ServicesSection';
-import ProductOffers from '../components/home/ProductOffers';
-import ProductTabs from '../components/home/ProductTabs';
-import { ProductBanner } from '../components/home/ProductBanner';
-import { NewsSection } from '../components/home/NewsSection';
+import React from "react";
+import ServicesSection from "../components/home/ServicesSection";
+import ProductOffers from "../components/home/ProductOffers";
+import ProductTabs from "../components/home/ProductTabs";
+import { ProductBanner } from "../components/home/ProductBanner";
+import { NewsSection } from "../components/home/NewsSection";
 
 const HomePage: React.FC = () => {
-    return (
-        <>
-            {/* Hero Carousel */}
-            <HeroSlider />
+  return (
+    <>
+      {/* Banner quảng cáo (dynamic từ API) */}
+      <ProductBanner />
 
-            {/* Banner quảng cáo (dynamic từ API) */}
-            <ProductBanner />
+      {/* Services Section */}
+      <ServicesSection />
 
-            {/* Services Section */}
-            <ServicesSection />
+      {/* Product Offers */}
+      <ProductOffers />
 
-    
-          
+      {/* Tin tức */}
+      <NewsSection />
 
-            {/* Product Offers */}
-            <ProductOffers />
-
-      
-       
-
-            {/* Tin tức */}
-            <NewsSection />
-
-            {/* Our Products Tabs */}
-            <ProductTabs />
-        </>
-    );
+      {/* Our Products Tabs */}
+      <ProductTabs />
+    </>
+  );
 };
 
 export default HomePage;
